7fd1339a5b8f75ea39e9b3eafd418ef7532ac2ab
Yejin Yoon <jenuar2470@gmail.com>
2023-06-02 11:04:06 +0900

    7th commit

--- page.js
+++ page.js
@@ -1,14 +1,23 @@
-//인기 영화 데이터
-
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTExODAzMzVhNGE2NzMxMTJlOTg1ZDQzZTUxMjMyYyIsInN1YiI6IjY0NzU3YWViOTI0Y2U2MDExNmM1ZmM3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LsITjGVhhqpYuf7KH_NEVSX7r45y_7tMtZ7OFN3UgCk'
-    }
-};
+window.onload = function(){
+	//실행될 코드
+    
+  
 
 
+
+ //영화 검색 이벤트 핸들러
+        //hadleSearch함수를 addEventListener에 전달
+        // function handleSearch(event) {
+        //     event.preventDefault(); //폼 제출 이벤트 막기
+        // }
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTExODAzMzVhNGE2NzMxMTJlOTg1ZDQzZTUxMjMyYyIsInN1YiI6IjY0NzU3YWViOTI0Y2U2MDExNmM1ZmM3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LsITjGVhhqpYuf7KH_NEVSX7r45y_7tMtZ7OFN3UgCk'
+            }
+        };
 
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => {
@@ -18,14 +27,11 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
         //가져온 데이터 사용
         let movies = data.results;
 
-        //영화 검색 이벤트 핸들러
-        //hadleSearch함수를 addEventListener에 전달
-        function handleSearch(event) {
-            event.preventDefault(); //폼 제출 이벤트 막기
-        }
+       
 
         //검색어 입력값 가져오기
         var searchInput = document.getElementById('search-input').value;
+        console.log("searchInput:", searchInput);
 
 
         //영화 검색, 대소문자 구분 안함
@@ -39,8 +45,8 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
 
 
         // 검색 이벤트 리스너 등록
-        var searchForm = document.querySelector('.search');
-        searchForm.addEventListener('submit', handleSearch);
+        // var searchForm = document.querySelector('.search');
+        // searchForm.addEventListener('submit', handleSearch);
 
 
         //영화 카드 리스트 UI 업데이트
@@ -49,6 +55,7 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
             cardContainer.innerHTML = '';  //기존 카드 제거
 
             movies.forEach(function (movie) {
+                var id = movie.id;
                 var title = movie.title;
                 var overview = movie.overview;
                 var posterPath = movie.poster_path;
@@ -65,11 +72,11 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
                 var newCard = document.createElement('div');
                 newCard.className = 'movie-card';
                 newCard.innerHTML = `
-        <img src="${imgPath}" alt="${title}" />
-        <h2>${title}</h2>
-        <p>${overview}</p>
-        <span>평점: ${voteAverage}</span>
-      `;
+                    <img src="${imgPath}" alt="${title}" data-id="${id}"/>
+                    <h2>${title}</h2>
+                    <p>${overview}</p>
+                    <span>평점: ${voteAverage}</span>
+                `;
 
                 //카드를 카드 컨테이너에 추가
                 cardContainer.appendChild(newCard);
@@ -84,23 +91,123 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
 
         }
 
+        // 영화 이미지 클릭 이벤트 핸들러
+        function handleImageClick(event) {
+            console.log(event.target)
+            const movieId = event.target.dataset.id; //클릭한 이미지의 영화 id 가져오기
+            alert('ID: ' + movieId);
+
+        }
 
 
         // 인기 영화 목록 초기 표시
         renderMovieCards(movies);
 
-    })
-    .catch((error) => {
-        console.log('Error:', error);
     });
 
-//알림 기능
-function showAlert() {
-    alert('검색 버튼을 클릭하면 영화 목록이 검색어로 필터링됩니다.');
+
+
+    document.getElementById("search-btn").addEventListener("click", function(){
+        const serachInput = document.getElementById("search-input").value;
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTExODAzMzVhNGE2NzMxMTJlOTg1ZDQzZTUxMjMyYyIsInN1YiI6IjY0NzU3YWViOTI0Y2U2MDExNmM1ZmM3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LsITjGVhhqpYuf7KH_NEVSX7r45y_7tMtZ7OFN3UgCk'
+            }
+        };
+
+fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        //가져온 데이터 사용
+        let movies = data.results;
+
+       
+
+        //검색어 입력값 가져오기
+        var searchInput = document.getElementById('search-input').value;
+        console.log("searchInput:", searchInput);
+
+
+        //영화 검색, 대소문자 구분 안함
+        var searchResults = movies.filter(movie =>
+            movie.title.toLowerCase().includes(searchInput.toLowerCase())
+        );
+
+        //영화 카드 리스트 UI 업데이트
+        //searchResults배열은 검색어와 일치하는 영화
+        renderMovieCards(searchResults);
+
+
+        // 검색 이벤트 리스너 등록
+        // var searchForm = document.querySelector('.search');
+        // searchForm.addEventListener('submit', handleSearch);
+
+
+        //영화 카드 리스트 UI 업데이트
+        function renderMovieCards(movies) {
+            var cardContainer = document.querySelector('.card-container');
+            cardContainer.innerHTML = '';  //기존 카드 제거
+
+            movies.forEach(function (movie) {
+                // The Godfather, The Shawshank Redemption
+                // filter, includes => 단어 일부로 검색 가능
+                if(searchInput === movie.title){
+                    
+                    var id = movie.id;
+                    var title = movie.title;
+                    var overview = movie.overview;
+                    var posterPath = movie.poster_path;
+                    var voteAverage = movie.vote_average;
+                    var movieId = movie.id; //영화 id 추가
+    
+    
+    
+                    //이미지 주소
+                    var imgPath = 'https://image.tmdb.org/t/p/w500' + posterPath;
+    
+    
+                    //새로운 카드 요소 생성
+                    var newCard = document.createElement('div');
+                    newCard.className = 'movie-card';
+                    newCard.innerHTML = `
+                        <img src="${imgPath}" alt="${title}" data-id="${id}"/>
+                        <h2>${title}</h2>
+                        <p>${overview}</p>
+                        <span>평점: ${voteAverage}</span>
+                    `;
+    
+                    //카드를 카드 컨테이너에 추가
+                    cardContainer.appendChild(newCard);
+                }               
+            });
+
+            //영화 이미지 클릭 이벤트 리스너 등록
+            const movieImages = document.querySelectorAll('.movie-card img');
+            movieImages.forEach((image) => {
+                image.addEventListener('click', handleImageClick);
+            });
+
+        }
+
+        // 영화 이미지 클릭 이벤트 핸들러
+        function handleImageClick(event) {
+            console.log(event.target)
+            const movieId = event.target.dataset.id; //클릭한 이미지의 영화 id 가져오기
+            alert('ID: ' + movieId);
+
+        }
+
+
+        // 인기 영화 목록 초기 표시
+        renderMovieCards(movies);
+
+    });
+      })
+    
 }
-
-//알림 버튼 이벤트 리스너 등록
-const alertButton = document.getElementById('alert-button');
-alertButton.addEventListener('click', showAlert);
-
 
